57a8fed4f0fc2a5d6356aee7863a88c573e59905
neamaAabdelzaher <neamaahmed25295@gmail.com>
2024-09-08 00:13:22 +0300

    blogs

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,6 @@ Route::get('/login', function () {
 Route::get('/register', function () {
     return view('auth.register');
 });
+Route::get('/blogs', function () {
+    return view('partial-components.blogs');
+});
