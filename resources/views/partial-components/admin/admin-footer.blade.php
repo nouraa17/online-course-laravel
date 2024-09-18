e670da7c3f576d9feec4ad423b0847d6748dd6a8
neamaAabdelzaher <neamaahmed25295@gmail.com>
2024-09-18 18:06:04 +0300

    update

---
+++ resources/views/partial-components/admin/admin-footer.blade.php
@@ -0,0 +1,59 @@
+</div>
+<!-- /.content-wrapper -->
+<footer class="main-footer pl-5">
+    <strong>Copyright &copy;  <a href="/"></a>Dr. Islam Osama</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Admin Dashboard</b>
+    </div>
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('dashboard_assets/plugins/jquery/jquery.min.js')}}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{asset('dashboard_assets/plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('dashboard_assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{asset('dashboard_assets/plugins/chart.js')}}/Chart.min.js')}}"></script>
+<!-- Sparkline -->
+<script src="{{asset('dashboard_assets/plugins/sparklines/sparkline.js')}}"></script>
+<!-- JQVMap -->
+<script src="{{asset('dashboard_assets/plugins/jqvmap/jquery.vmap.min.js')}}"></script>
+<script src="{{asset('dashboard_assets/plugins/jqvmap/maps/jquery.vmap.usa.js')}}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{asset('dashboard_assets/plugins/jquery-knob/jquery.knob.min.js')}}"></script>
+<!-- daterangepicker -->
+<script src="{{asset('dashboard_assets/plugins/moment/moment.min.js')}}"></script>
+<script src="{{asset('dashboard_assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="{{asset('dashboard_assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
+<!-- Summernote -->
+<script src="{{asset('dashboard_assets/plugins/summernote/summernote-bs4.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{asset('dashboard_assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('dashboard_assets/dist/js/adminlte.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+{{-- <script src="{{asset('dashboard_assets/dist/js/demo.js')}}"></script> --}}
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{asset('dashboard_assets/dist/js/pages/dashboard.js')}}"></script>
+
+
+
+</body>
+</html>
+
+
---
+++ resources/views/partial-components/admin/admin-sidebar.blade.php
@@ -0,0 +1,144 @@
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="" class="brand-link">
+                <i class="fa fa-book me-3"></i>
+                <span class="brand-text font-weight-light">Dr. Islam Osama</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="{{asset ('dashboard_assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">Islam Osama</a>
+                    </div>
+                </div>
+
+
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+
+                        <li class="nav-item menu-open ">
+                            <a href="" class="nav-link bg-primary ">
+                                {{-- <i class="nav-icon fas fa-tachometer-alt"></i> --}}
+                                <p>
+                               Users
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{route('dashboard.users.index')}}" class="nav-link {{request()->is('dashboard/users/index') ? 'active' : ''}} ">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>All Users</p>
+                                    </a>
+                                </li>
+
+
+                            </ul>
+                        </li>
+
+
+                    </ul>
+
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+
+                        <li class="nav-item menu-open">
+                            <a href="" class="nav-link bg-primary ">
+                                {{-- <i class="nav-icon fas fa-tachometer-alt"></i> --}}
+                                <p>
+                               Blogs
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{route('dashboard.blogs.index')}}" class="nav-link {{request()->is('dashboard/blogs/index') ? 'active' : ''}} ">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>All Blogs</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{route('dashboard.blogs.create')}}" class="nav-link {{request()->is('dashboard/blogs/create') ? 'active' : ''}} ">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>Create Blog</p>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
+
+
+                    </ul>
+
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+
+                        <li class="nav-item menu-open ">
+                            <a href="#" class="nav-link bg-primary">
+                                {{-- <i class="nav-icon fas fa-tachometer-alt"></i> --}}
+                                <p>
+                                    Categories
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="" class="nav-link active">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>All Categories</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>Create Categories</p>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
+                        <li class="nav-item menu-open ">
+                            <a href="#" class="nav-link bg-primary">
+                                {{-- <i class="nav-icon fas fa-tachometer-alt"></i> --}}
+                                <p>
+                               Courses
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="" class="nav-link active">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>All Courses</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link">
+                                        {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                        <p>Create Course</p>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
+
+
+                    </ul>
+
+
+
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
